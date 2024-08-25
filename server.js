c9ca47fcf6f140ff95955119404b88ce01d987ee
Alexander <alexanderl96@yahoo.com>
2024-08-25 18:30:27 -0400

    adding filers

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 9001;
+
+app.get('/api', (req, res) => {
+  res.json({ message: 'Hello from Express!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
