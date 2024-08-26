e20e3f30a6ea88e5e34bbb2674b6d4de5c513c68
Alexander <alexanderl96@yahoo.com>
2024-08-26 11:30:54 -0400

    Fixing bugs for endpoints

--- app.js
+++ app.js
@@ -110,6 +110,16 @@ app.use('/generate_NumbersDay', generateNumbersDay);
 
 app.use('/', homeRoot);
 
+
+
+app.all('/*', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-Width')
+  next();
+});
+
+
 app.use(function(req, res, next) {
     next(createError(404));
   });
--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 
-// router.get('/', (req, res) => {
-//     res.json({ message: 'Hello from Express!' });
-// });
+router.get('/', (req, res) => {
+    res.json({ message: 'Hello from Express!' });
+});
 
 
-router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  });
+// router.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+//   });
 
 
 module.exports = router;
