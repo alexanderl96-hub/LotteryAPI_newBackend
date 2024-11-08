576a43f77ce0c7608d7fcc27242d4fefe4a06075
Alexander <alexanderl96@yahoo.com>
2024-11-08 16:20:03 -0500

    addibng more changes

--- server.js
+++ server.js
@@ -3,8 +3,14 @@ require("dotenv").config();
 const PORT = process.env.PORT || 9001;
 
 
-    app.listen(PORT, () => {
-        console.log(`App listening on port : ${PORT} this is the last checking`);
+    // app.listen(PORT, () => {
+    //     console.log(`App listening on port : ${PORT} this is the last checking`);
 
        
+    // });
+    const server = app.listen(PORT, () => {
+        console.log(`App listening on port : ${PORT} with a custom timeout`);
     });
+    
+    // Set the server timeout to 5 minutes (300000 milliseconds)
+    server.setTimeout(300000); // Adjust this value as needed
